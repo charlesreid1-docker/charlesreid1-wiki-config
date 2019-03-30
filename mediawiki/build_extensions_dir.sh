26cf6df6f592808693ada3eca3bf64d455d1d028
C Reid <charlesreid1@Gmail.com>
2019-03-29 20:26:43 -0700

    update branches used in build_extensions script

--- mediawiki/build_extensions_dir.sh
+++ mediawiki/build_extensions_dir.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# 
+# clone or download each extension
+# and build o
 
 mkdir -p extensions
 (
@@ -10,11 +13,20 @@ Extension="SyntaxHighlight_GeSHi"
 if [ ! -d ${Extension} ]
 then
     ## This requires mediawiki > 1.31
+    ## (so does REL1_31)
     #git clone https://github.com/wikimedia/mediawiki-extensions-SyntaxHighlight_GeSHi.git SyntaxHighlight_GeSHi
 
-    wget https://extdist.wmflabs.org/dist/extensions/SyntaxHighlight_GeSHi-REL1_30-87392f1.tar.gz -O SyntaxHighlight_GeSHi.tar.gz
-    tar -xzf SyntaxHighlight_GeSHi.tar.gz -C ${PWD}
-    rm -f SyntaxHighlight_GeSHi.tar.gz
+    ## This manually downloads REL1_30
+    #wget https://extdist.wmflabs.org/dist/extensions/SyntaxHighlight_GeSHi-REL1_30-87392f1.tar.gz -O SyntaxHighlight_GeSHi.tar.gz
+    #tar -xzf SyntaxHighlight_GeSHi.tar.gz -C ${PWD}
+    #rm -f SyntaxHighlight_GeSHi.tar.gz
+
+    # Best of both worlds
+    git clone https://github.com/wikimedia/mediawiki-extensions-SyntaxHighlight_GeSHi.git SyntaxHighlight_GeSHi
+    (
+    cd ${Extension}
+    git checkout REL1_30
+    )
 else
     echo "Skipping ${Extension}"
 fi
@@ -25,6 +37,10 @@ Extension="ParserFunctions"
 if [ ! -d ${Extension} ]
 then
     git clone https://github.com/wikimedia/mediawiki-extensions-ParserFunctions.git ${Extension}
+    (
+    cd ${Extension}
+    git checkout REL1_31
+    )
 else
     echo "Skipping ${Extension}"
 fi
@@ -35,6 +51,10 @@ Extension="EmbedVideo"
 if [ ! -d ${Extension} ]
 then
     git clone https://github.com/HydraWiki/mediawiki-embedvideo.git ${Extension}
+    (
+    cd ${Extension}
+    git checkout v2.7.3
+    )
 else
     echo "Skipping ${Extension}"
 fi
@@ -47,7 +67,7 @@ then
     git clone https://github.com/wikimedia/mediawiki-extensions-Math.git ${Extension}
     (
     cd ${Extension}
-    git checkout REL1_30
+    git checkout REL1_31
     )
 else
     echo "Skipping ${Extension}"
